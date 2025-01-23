89dd3787159952cfcb8d62effafd86fadf0d893a
Yuuna Ishihara <202411ap01_ishihara@x-ide.sakura.ne.jp>
2025-01-23 14:57:14 +0900

    #34058：登録している項目ではないランクが選択されている

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -255,9 +255,17 @@ function subStockEditView($param)
 				<th>ランク</th>
 				<td>
 					<?php
+					// stockNoの有無で新規登録時のデフォルト値を設定するとともに、DBに登録されている値とfor文の変数$iがチェックを付ける値で一致した場合にチェックを付ける処理を追加する
+					if (!$param["stockNo"]) {
+						$param["rank"] = 1;
+					}
 					for ($i = 0; $i < 5; $i++) {
+						$check = '';
+						if (($param["rank"] - 1) == $i) {
+							$check = 'checked = "checked"';
+						}
 					?>
-						<input type="radio" name="rank" value="<?php print $i + 1; ?>" <?php if ($param["rank"] == $i) print ' checked="checked"'; ?> /> <?php print fnRankName($i); ?>
+						<input type="radio" name="rank" value="<?php print $i + 1; ?>" <?php print $check; ?> /> <?php print fnRankName($i); ?>
 					<?php
 					}
 					?>
