22744df4e78bf6740c8aa142480d726e81fdb98b
Yuuna Ishihara <202411ap01_ishihara@x-ide.sakura.ne.jp>
2025-01-23 15:34:03 +0900

    #34075：距離　登録している内容ではないラジオボタンが選択されている

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -295,9 +295,17 @@ function subStockEditView($param)
 				<th>距離</th>
 				<td>
 					<?php
+					// stockNoの有無で新規登録時のデフォルト値を設定するとともに、DBに登録されている値とfor文の変数$iがチェックを付ける値で一致した場合にチェックを付ける処理を追加する
+					if (!$param["stockNo"]) {
+						$param["distance"] = 1;
+					}
 					for ($i = 0; $i < 4; $i++) {
+						$check = '';
+						if (($param["distance"] - 1) == $i) {
+							$check = 'checked = "checked"';
+						}
 					?>
-						<input type="radio" name="distance" value="<?php print $i + 1; ?>" <?php if ($param["distance"] == $i) print ' checked="checked"'; ?> /> <?php print fnDistanceName($i); ?>
+						<input type="radio" name="distance" value="<?php print $i + 1; ?>" <?php print $check; ?> /><?php print fnDistanceName($i); ?>
 					<?php
 					}
 					?>
